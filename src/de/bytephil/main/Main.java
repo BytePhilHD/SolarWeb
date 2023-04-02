dcd3a8ee92187268be2805fb53b8a5cb26b8e64f
BytePhil <phitho2018@gmail.com>
2023-04-02 13:22:01 +0200

    Working on the readout

--- src/de/bytephil/main/Main.java
+++ src/de/bytephil/main/Main.java
@@ -1,6 +1,7 @@
 package de.bytephil.main;
 
 import de.bytephil.enums.MessageType;
+import de.bytephil.services.FileReader;
 import de.bytephil.services.LogService;
 import de.bytephil.services.RestAPI;
 import de.bytephil.services.Schedular;
@@ -9,6 +10,7 @@ import io.javalin.Javalin;
 import io.javalin.http.staticfiles.Location;
 import org.apache.commons.logging.Log;
 
+import java.lang.reflect.Field;
 import java.util.Timer;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
@@ -43,6 +45,16 @@ public class Main {
                 ctx.send("P_AC: " + RestAPI.data[2]);
                 ctx.send("Yield_Total: " + RestAPI.data[6]);
                 ctx.send("Yield_Day: " + RestAPI.data[7]);
+                   /*
+                   ctx.send("STATS-Mo: " + FileReader.readData.get("32.03.2023"));
+                   ctx.send("STATS-Di: " + FileReader.readData.get("31.03.2023"));
+                   ctx.send("STATS-Mi: " + FileReader.readData.get("30.03.2023"));
+                   ctx.send("STATS-Do: " + FileReader.readData.get("29.03.2023"));
+                   ctx.send("STATS-Fr: " + FileReader.readData.get("28.03.2023"));
+                   ctx.send("STATS-Sa: " + FileReader.readData.get("27.03.2023"));
+                   ctx.send("STATS-So: " + FileReader.readData.get("26.03.2023"));
+
+                    */
             });
             ws.onClose(ctx -> {
                 Console.printout("User disconnected from main websocket. (IP: " + ctx.session.getRemoteAddress().getAddress().toString().replace("/", "") + ")", MessageType.INFO);
@@ -52,7 +64,29 @@ public class Main {
                    ctx.send("P_AC: " + RestAPI.data[2]);
                    ctx.send("Yield_Total: " + RestAPI.data[6]);
                    ctx.send("Yield_Day: " + RestAPI.data[7]);
-                   ctx.send("STATS-Mo: 20000");
+                   /*
+                   ctx.send("STATS-Mo: " + FileReader.readData.get("32.03.2023"));
+                   ctx.send("STATS-Di: " + FileReader.readData.get("31.03.2023"));
+                   ctx.send("STATS-Mi: " + FileReader.readData.get("30.03.2023"));
+                   ctx.send("STATS-Do: " + FileReader.readData.get("29.03.2023"));
+                   ctx.send("STATS-Fr: " + FileReader.readData.get("28.03.2023"));
+                   ctx.send("STATS-Sa: " + FileReader.readData.get("27.03.2023"));
+                   ctx.send("STATS-So: " + FileReader.readData.get("26.03.2023"));
+
+                    */
+                   ctx.send("STATS-Mo: " + 1000);
+                   ctx.send("STATS-Di: " + 2000);
+                   ctx.send("STATS-Mi: " + 3000);
+                   ctx.send("STATS-Do: " + 4000);
+                   ctx.send("STATS-Fr: " + 5000);
+                   ctx.send("STATS-Sa: " + 6000);
+                   ctx.send("STATS-So: " + 7000);
+                   System.out.println(FileReader.readData.get("32.03.2023"));
+                   System.out.println(FileReader.readData.get("31.03.2023"));
+                   System.out.println(FileReader.readData.get("30.03.2023"));
+                   System.out.println(FileReader.readData.get("29.03.2023"));
+                   System.out.println(FileReader.readData.get("28.03.2023"));
+                   System.out.println(FileReader.readData.get("27.03.2023"));
                }
             });
         });
--- src/de/bytephil/services/FileReader.java
+++ src/de/bytephil/services/FileReader.java
@@ -2,8 +2,10 @@ package de.bytephil.services;
 
 import java.io.*;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
+import java.util.List;
 
 public class FileReader {
 
@@ -11,21 +13,41 @@ public class FileReader {
 
 
     public static void readFile() throws IOException {
+        HashMap<String, Integer> unhandledData = new HashMap<>();
         String fileName = "logs/log.txt";
         File file = new File(fileName);
         FileInputStream fis = new FileInputStream(file);
         InputStreamReader isr = new InputStreamReader(fis);
         BufferedReader br = new BufferedReader(isr);
 
-        readData.clear();
+
         String line;
         while((line = br.readLine()) != null){
             //process the line
             String date = line.substring(1, 11);
             date = date.substring(8, 10) + "." + date.substring(5, 7) + "." + date.substring(0, 4);
             int wh = Integer.parseInt(line.substring(35, 39));
-            readData.put(date, wh);
+            unhandledData.put(date, wh);
         }
         br.close();
+
+        readData.clear();
+
+        if (unhandledData.size() >= 7) {
+            List<String> keys = new ArrayList<>(unhandledData.keySet());
+            int size = keys.size();
+            int count = 0;
+            for (int i = size - 1; i >= 0 && count < 7; i--) {
+                String key = keys.get(i);
+                int value = unhandledData.get(key);
+                readData.put(key, value);
+                System.out.println(key + ", " + value);
+                count++;
+            }
+        } else {
+           // TODO Wenn weniger als 7 Einträge drin sind
+            System.out.println("ERROR");
+        }
+
     }
 }
